f6218f03c87c7412674fd5eb8d7eb70a03a22a38
Can AYISIK <canayisik@gmail.com>
2020-12-22 13:33:44 +0300

    Initial Commit

---
+++ MetaWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetaWeatherApp
+//
+//  Created by Can Ayışık on 22.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
